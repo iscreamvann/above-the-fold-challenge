cddabba2a18445312e48700e3fa2bc3c9998ab73
Kyle Vann <kyle.vann1996@icloud.com>
2024-10-23 09:09:27 +0100

    Nav fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -58,19 +58,14 @@ const UlStyled = styled.ul`
   }
 
   @media (min-width: 769px) {
-    justify-content: space-between;
+    justify-content: flex-end;
     padding: 0 20px;
-
-    & li {
-      margin-left: 20px;
-    }
 
     & li:last-child {
       margin-left: 20px;
     }
   }
 `;
-
 
 const LiStyled = styled.li`
   margin-right: 8px;
